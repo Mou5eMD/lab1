f0706850fc55076ddd8c91c2fada63a604660a84
Mouse <mixa.vayder@yandex.ru>
2021-06-06 22:49:17 +0300

    tt

---
+++ googlemaps/src/main/java/com/mirea/mikhailpiskunov/googlemaps/MapsActivity.java
@@ -0,0 +1,84 @@
+package com.mirea.mikhailpiskunov.googlemaps;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
+
+    private static final int TAG_CODE_PERMISSION_LOCATION = 1;
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        LatLng mirea = new LatLng(55.670005, 37.479894);
+        CameraPosition cameraPosition = new CameraPosition.Builder().target(
+                mirea).zoom(12).build();
+        mMap.animateCamera(CameraUpdateFactory
+                .newCameraPosition(cameraPosition));
+        mMap.addMarker(new MarkerOptions().title("МИРЭА")
+                .snippet("Крупнейший политехнический ВУЗ").position(mirea));
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(mirea));
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{
+                            Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION},
+                    TAG_CODE_PERMISSION_LOCATION);
+        }
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.setTrafficEnabled(true);
+
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case TAG_CODE_PERMISSION_LOCATION:
+                mMap.setMyLocationEnabled(true);
+                break;
+        }
+    }
+    @Override
+    public void onMapClick(LatLng latLng) {
+
+        CameraPosition cameraPosition = new CameraPosition.Builder().target(
+                latLng).zoom(12).build();
+        mMap.animateCamera(CameraUpdateFactory
+                .newCameraPosition(cameraPosition));
+        mMap.addMarker(new MarkerOptions().title("Где я?")
+                .snippet("Новое место").position(latLng));
+    }
+
+}
+
---
+++ yandexmaps/src/main/java/com/mirea/mikhailpiskunov/yandexmaps/MainActivity.java
@@ -0,0 +1,85 @@
+package com.mirea.mikhailpiskunov.yandexmaps;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.graphics.PointF;
+import android.os.Bundle;
+
+import com.yandex.mapkit.Animation;
+import com.yandex.mapkit.MapKit;
+import com.yandex.mapkit.MapKitFactory;
+import com.yandex.mapkit.geometry.Point;
+import com.yandex.mapkit.layers.ObjectEvent;
+import com.yandex.mapkit.map.CameraPosition;
+import com.yandex.mapkit.map.IconStyle;
+import com.yandex.mapkit.mapview.MapView;
+import com.yandex.mapkit.user_location.UserLocationLayer;
+import com.yandex.mapkit.user_location.UserLocationObjectListener;
+import com.yandex.mapkit.user_location.UserLocationView;
+import com.yandex.runtime.image.ImageProvider;
+
+public class MainActivity extends AppCompatActivity implements UserLocationObjectListener {
+    private MapView mapView;
+    private final String MAPKIT_API_KEY = "71cb2cb5-f77f-497b-84ce-4431e7535fb7";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        MapKitFactory.setApiKey(MAPKIT_API_KEY);
+        MapKitFactory.initialize(this);
+        setContentView(R.layout.activity_main);
+        mapView = findViewById(R.id.mapview);
+        mapView.getMap().move(
+                new CameraPosition(new Point(55.751574, 37.573856), 11.0f, 0.0f, 0.0f),
+                new Animation(Animation.Type.SMOOTH, 0),
+                null);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+        MapKitFactory.getInstance().onStop();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+        MapKitFactory.getInstance().onStart();
+
+    }
+
+    @Override
+    public void onObjectAdded(@NonNull UserLocationView userLocationView) {
+
+        MapKit mapKit = MapKitFactory.getInstance();
+        UserLocationLayer userLocationLayer = mapKit.createUserLocationLayer(mapView.getMapWindow());
+        userLocationLayer.setVisible(true);
+        userLocationLayer.setHeadingEnabled(true);
+        userLocationLayer.setObjectListener(this);
+
+        userLocationLayer.setAnchor(
+                new PointF((float) (mapView.getWidth() * 0.5), (float) (mapView.getHeight() *
+                        0.5)),
+                new PointF((float) (mapView.getWidth() * 0.5), (float) (mapView.getHeight() *
+                        0.83)));
+        userLocationView.getArrow().setIcon(ImageProvider.fromResource(
+                this, android.R.drawable.star_big_on));
+        userLocationView.getPin().setIcon(ImageProvider.fromResource(
+                this, android.R.drawable.ic_menu_mylocation));
+        userLocationView.getAccuracyCircle().setFillColor(Color.BLUE);
+    }
+
+    @Override
+    public void onObjectRemoved(@NonNull UserLocationView userLocationView) {
+
+    }
+
+    @Override
+    public void onObjectUpdated(@NonNull UserLocationView userLocationView, @NonNull ObjectEvent objectEvent) {
+
+    }
+}
